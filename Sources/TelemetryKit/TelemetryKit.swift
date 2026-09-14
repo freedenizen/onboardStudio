93848ca72a62e9fe39980a38418a71e37e521acb
freedenizen <freedenizen@gmail.com>
2026-09-13 22:35:06 -0700

    ci: only trust GitHub-signed merge commits as signature fallback

--- Sources/TelemetryKit/TelemetryKit.swift
+++
@@ -1,5 +0,0 @@
-/// Time-indexed telemetry channels, interpolation, lap detection.
-/// Filled in during milestone M1.
-public enum TelemetryKitInfo {
-    public static let name = "TelemetryKit"
-}
